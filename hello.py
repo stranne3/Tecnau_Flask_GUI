7ffd8d50e3b60559eefb7a68e1293e02145d80d8
wolsing <97888603+wolsing@users.noreply.github.com>
2022-05-12 15:56:36 +0200

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+from flask import jsonify
+from time import time
+from flask import request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/count", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('start'):
+            for i in range(5,0,-1):
+                print(i)
+                time.sleep(1)
+                return render_template("index.html", i)
